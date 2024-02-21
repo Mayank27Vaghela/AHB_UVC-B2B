ecd4c0ef9de649865f4995a69eed01cd07c88440
Mayank <mayankvagela60@gamil.com>
2024-02-22 01:15:06 +0530

    code changed

--- AHB_UVC/SEQ/AHB_UVC_master_wr_seq.sv
+++ AHB_UVC/SEQ/AHB_UVC_master_wr_seq.sv
@@ -58,7 +58,7 @@ task AHB_UVC_master_wr_seq::body();
 
   trans_h = AHB_UVC_master_transaction_c::type_id::create("trans_h");
   start_item(trans_h);
-  if(!trans_h.randomize() with{haddr == 32'h38;hburst_type ==INCR4;hsize_type ==WORD;hwrite == 1;})begin
+  if(!trans_h.randomize() with{haddr == 32'h38;hburst_type ==INCR4;hsize_type ==WORD;hwrite == 0;})begin
      `uvm_error(get_type_name(),"Sequence Randomization failed");
   end
   //trans_h.print();
